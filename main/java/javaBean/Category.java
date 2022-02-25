7088788be1f5f32b869916750bb6c4033717e87b
lvq00001 <50162233+lvq00001@users.noreply.github.com>
2022-02-25 21:06:16 +0700

    Update Category.java

--- main/java/javaBean/Category.java
+++ main/java/javaBean/Category.java
@@ -3,14 +3,9 @@ package javaBean;
 import java.io.Serializable	;
 
 public class Category implements Serializable{
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
-	/**
-	 * 
-	 */
-	//private static final long serialVersionUID = 1L;
+	
 	private int parentId;
 	private int categoryId;
 	private String categoryName;
